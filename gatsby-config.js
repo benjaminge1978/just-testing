671efb00a8482c7e86ab36f1231c850d5edab691
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2020-01-13 20:02:16 +0100

    Updated for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,18 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Seventa Events | Event Planning & Management Agency`,
+        short_name: `Seventa Events`,
+        start_url: `/`,
+        background_color: `#333`,
+        theme_color: `#333`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,

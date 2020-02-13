19a5b11c44d50fc23281d612863239590ad5770a
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2020-02-13 12:37:18 +0100

    form-updates

--- src/components/contact/form/form.js
+++ src/components/contact/form/form.js
@@ -17,6 +17,12 @@ export default () => (
         <label>
             <textarea name="message" rows="6" placeholder="How can we help?" />
         </label>
-        <button type="submit" value="Send">Send message <Arrow /></button>
+
+            <input type="checkbox" name="consent" value="Consent" />
+<label for="vehicle1"><p className="consent">I have read and I accept the <a href="/privacy-policy">privacy policy</a> and I give my consent for the information above to be collected.</p>
+</label><br></br>
+
+
+        <button className="animated-link" type="submit" value="Send">Send message <Arrow /></button>
     </form>
 )

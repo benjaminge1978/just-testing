1f0253e04bf663c612b6b6a57e35fd9b9f2fe970
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2020-02-14 17:09:03 +0100

    SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,6 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
 const getData = graphql`
   query {
@@ -10,44 +10,44 @@ const getData = graphql`
         siteDesc: description
         author
         siteUrl
-        image
+        siteImage: image
         twitterUsername
       }
     }
   }
-`
+`;
 
-const SEO = ({ title, description }) => {
-  const { site } = useStaticQuery(getData)
+const SEO = ({ title, description, slug, image }) => {
+  const { site } = useStaticQuery(getData);
 
   const {
     siteDesc,
     siteTitle,
     siteUrl,
-    image,
-    twitterUsername,
-  } = site.siteMetadata
+    siteImage,
+    twitterUsername
+  } = site.siteMetadata;
 
   return (
     <Helmet htmlAttributes={{ lang: "en" }} title={`${title} | ${siteTitle}`}>
       <meta name="description" content={description || siteDesc} />
-      <meta name="image" content={image} />
+      <meta name="image" content={siteImage} />
       {/* facebook cards */}
-      <meta property="og:url" content={siteUrl} />
+      <meta property="og:url" content={`${siteUrl}/blog/${slug}` || siteUrl} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={siteTitle} />
-      <meta property="og:description" content={siteDesc} />
-      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:title" content={title || siteTitle} />
+      <meta property="og:description" content={description || siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${siteImage}`} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
-      <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={siteDesc} />
-      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      <meta name="twitter:title" content={title || siteTitle} />
+      <meta name="twitter:description" content={description || siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${siteImage}`} />i
     </Helmet>
-  )
-}
+  );
+};
 
-export default SEO
+export default SEO;

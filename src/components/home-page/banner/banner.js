b2bb059ade6a6ec60eaf0db1670a2606457cad30
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2020-02-16 23:15:38 +0100

    images res

--- src/components/home-page/banner/banner.js
+++ src/components/home-page/banner/banner.js
@@ -45,7 +45,7 @@ export default () => {
     query BannerImage {
       file(relativePath: { eq: "seventa-header.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 1400, quality: 90) {
+          fluid(maxWidth: 1400, quality: 80) {
             ...GatsbyImageSharpFluid
           }
         }

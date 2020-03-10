013b6bd507a813024778c71a073c958d2a4cd174
Ben Davidson <bendavidson@Bens-MacBook-Pro.local>
2020-03-10 22:45:02 +0100

    brand

--- src/components/site-brand/site-brand-footer.js
+++ src/components/site-brand/site-brand-footer.js
@@ -3,7 +3,7 @@ import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 
 import './site-brand.scss'
-import Logo from '../../images/seventa-light-logo.svg'
+import Logo from '../../images/seventa-light-logo-new.svg'
 
 export default () => (
     <AniLink to="/" className="site-brand" direction="up" bg="#FFFFFF" aria-label="Seventa">

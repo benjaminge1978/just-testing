444df59d961e46f243249e972fa4bd6eb87efd56
DecadentIpsum <decadentipsum@gmail.com>
2022-02-15 09:23:12 +0200

    feat: add external links on case-studies-items component

--- src/components/case-studies/case-studies-item.js
+++ src/components/case-studies/case-studies-item.js
@@ -1,9 +1,10 @@
-import React from "react"
-import Fade from "react-reveal/Fade"
-import PropsTyps from "prop-types"
-import Img from "gatsby-image"
-import Arrow from "../../images/arrow-right.svg"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
+import Fade from "react-reveal/Fade"
+import Img from "gatsby-image"
+import PropsTyps from "prop-types"
+import React from "react"
+
+import Arrow from "../../images/arrow-right.svg"
 
 const CaseStudiesItem = ({ title, thumbSrc, slug, excerpt, thumb }) => {
   const itemLink = `/${slug}`
@@ -11,7 +12,33 @@ const CaseStudiesItem = ({ title, thumbSrc, slug, excerpt, thumb }) => {
   return (
     <Fade bottom cascade distance="70px">
       <div className="case-studies-list-item">
-        <AniLink
+			{itemLink === "innovative-hire" ?
+			<>
+			<a href="https://www.innovativehire.com/" target="_blank" rel="noopener noreferrer" className="case-studies-list-item__thumb">
+          <Fade bottom cascade distance="70px">
+            <Img
+              fluid={thumb.fluid}
+              alt={thumb.title}
+              className="animated-img"
+            />
+          </Fade>
+        </a>
+        <h2 className="case-studies-list-item__title">
+          <a href="https://www.innovativehire.com/" target="_blank" rel="noopener noreferrer"className="animated-link">
+            {title}
+          </a>
+        </h2>
+        <div
+          className="case-studies-list-item__excerpt"
+          dangerouslySetInnerHTML={{ __html: excerpt }}
+        />
+        <AniLink cover to={itemLink} direction="up" bg="#FFFFFF">
+
+        </AniLink>
+				</>
+			:
+			<>
+			<AniLink
           cover
           to={itemLink}
           direction="up"
@@ -38,6 +65,8 @@ const CaseStudiesItem = ({ title, thumbSrc, slug, excerpt, thumb }) => {
         <AniLink cover to={itemLink} direction="up" bg="#FFFFFF">
 
         </AniLink>
+				</>
+			}
       </div>
     </Fade>
   )
